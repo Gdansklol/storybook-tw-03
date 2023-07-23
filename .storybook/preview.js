b5ca04ec802438975ac2188d2b80492cb061dafd
Båmi Lee <swedenebomi@gmail.com>
2023-07-23 23:05:38 +0200

    changes preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,28 @@
-/** @type { import('@storybook/react').Preview } */
-const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
+// /** @type { import('@storybook/react').Preview } */
+
+// const preview = {
+//   parameters: {
+//     actions: { argTypesRegex: "^on[A-Z].*" },
+//     controls: {
+//       matchers: {
+//         color: /(background|color)$/i,
+//         date: /Date$/,
+//       },
+//     },
+//   },
+// };
+
+// export default preview;
+
+
+import "../src/styles/tailwind.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
     },
-  },
-};
-
-export default preview;
+}
